bde28ee73dba52ccfc8c4b3d2e67e298f5866787
Jan Kalfirt <kalfja@gmail.com>
2021-10-22 16:17:22 +0200

    new

--- WebServiceAutomation/GetEndpoint/TestGetEndpoint.cs
+++ WebServiceAutomation/GetEndpoint/TestGetEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -62,6 +63,7 @@ namespace WebServiceAutomation.GetEndpoint
             Task<HttpResponseMessage> httpResponseMessage = httpclient.GetAsync(getUri);
             var result1 = httpResponseMessage.Result.ToString();
             Console.WriteLine(String.Format("Get all endpoints with URI {0}", result1));
+
             //Close connection
             httpclient.Dispose();
             Assert.IsTrue(result1.Contains("StatusCode: 200"));
@@ -70,8 +72,8 @@ namespace WebServiceAutomation.GetEndpoint
             HttpContent responsedata= httpResponseMessage.Result.Content;
             HttpStatusCode statusCode = httpResponseMessage.Result.StatusCode;
             Console.WriteLine("The content of the message is:"+responsedata.ToString());
-            Console.WriteLine("The Status code is:" + statusCode.ToString());
-
+            Console.WriteLine("The Status code is:" + statusCode);
+            Console.WriteLine("The Status code is:" + (int)statusCode);
             Task<string> reponse1 = responsedata.ReadAsStringAsync();
             string data= reponse1.Result;
             Console.WriteLine(data);
@@ -111,7 +113,7 @@ namespace WebServiceAutomation.GetEndpoint
             HttpClient httpclient = new HttpClient();
 
             var requestheader = httpclient.DefaultRequestHeaders;
-            requestheader.Add("Accept", "application/xml");
+            requestheader.Add("Accept", "application/json");
 
             Task<HttpResponseMessage> httpResponseMessage = httpclient.GetAsync(getUrl);
             var result = httpResponseMessage.Result;
@@ -171,16 +173,52 @@ namespace WebServiceAutomation.GetEndpoint
             Console.WriteLine(data);
             //Close connection
             httpclient.Dispose();
+        }
+       
+        
+        [TestMethod]
+        public void _8TestUsingStatement()
+        {
+            using (HttpClient httpClient= new HttpClient())
+            {
+                using (HttpRequestMessage httpRequestMessage = new HttpRequestMessage())
+                {
+                    
+                    httpRequestMessage.RequestUri = new Uri(getUrl);
+                    httpRequestMessage.Method = HttpMethod.Get;
+                    httpRequestMessage.Headers.Add("Accept", "application/json");
+
+                    Task<HttpResponseMessage> httpResponse = httpClient.GetAsync(getUrl);
+
+                    using (HttpResponseMessage httpResponseMessage= httpResponse.Result)
+                    {
+                         
+                        Console.WriteLine(httpResponseMessage);
+
+                        //Status Code
+                        HttpStatusCode statusCode = httpResponseMessage.StatusCode;
+                        Console.WriteLine("Status Code= "+ statusCode);
+                        Console.WriteLine("Status Code= " + (int)statusCode);
 
 
+                        //Reponse Data
+                        HttpContent responsedata = httpResponseMessage.Content;
+                        Task<string> response= responsedata.ReadAsStringAsync();
+                        string data =response.Result;
+                        Console.WriteLine(data);
 
 
+                        JsonRootObject jsonRootObject= JsonConvert.DeserializeObject<JsonRootObject>(response.ResponseContent)
+                    }
+                }
+            }
         }
 
+        [TestMethod]
+        public void _9TestDeserializationofJsonResponse()
+        {
 
-
-
-
+        }
 
 
 
